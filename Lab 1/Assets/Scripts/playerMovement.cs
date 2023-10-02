9a27e78f4f1d84e9f33916ef60b278187524c229
DarraghMcKernan <dmckernan77@gmail.com>
2023-10-02 15:30:35 +0100

    push to work at home

---
+++ Lab 1/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public int speed;
+    public Vector2 movement;
+    public Vector2 bulletSpeed;
+    public Rigidbody2D rg;
+    public GameObject bullet;
+    const float MAX_COOLDOWN = 10.0f;
+    float cooldownTimer = 0;
+
+
+    private void Update()
+    {
+        if(GameManagerScript.instance.gameOver==false)
+        {
+            float deltaX = Input.GetAxisRaw("Horizontal");
+            float deltaY = Input.GetAxisRaw("Vertical");
+            movement = new Vector2(deltaX, deltaY);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (cooldownTimer <= 0)
+                {
+                    cooldownTimer = MAX_COOLDOWN;
+                    Shoot();
+                }
+            }
+        }
+        if(GameManagerScript.instance.gameOver == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                GameManagerScript.instance.gameOver = false;
+            }
+            rg.transform.position = new Vector3(0.0f, -4.0f, 0.0f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rg.MovePosition(rg.position + movement * speed * Time.fixedDeltaTime);
+
+        cooldownTimer--; ;
+        if(cooldownTimer < 0)
+        {
+            cooldownTimer = 0;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LeftWall"))
+        {
+            rg.transform.position = new Vector3(10.0f, rg.transform.position.y, 0.0f);
+        }
+        if (collision.gameObject.CompareTag("RightWall"))
+        {
+            rg.transform.position = new Vector3(-10.0f, rg.transform.position.y, 0.0f);
+        }
+    }
+
+
+    void Shoot()
+    {
+        GameObject playersBullet = Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+        playersBullet.GetComponent<Rigidbody2D>().velocity = bulletSpeed;
+        // destroy bullet after one and a  half of seconds
+        Destroy(playersBullet, 2.5f);
+    }
+}
