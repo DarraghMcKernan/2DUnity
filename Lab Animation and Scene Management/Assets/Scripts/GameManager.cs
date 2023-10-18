6f9c3c0a96c8bdb143b644ec897fac44660c6a91
DarraghMcKernan <dmckernan77@gmail.com>
2023-10-18 15:15:55 +0100

    all player movement done

---
+++ Lab Animation and Scene Management/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    //example   
+    public int difficultyLevel;
+    private void Awake()
+    {
+        //numtimes = 0;
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
+}
---
+++ Lab Animation and Scene Management/Assets/Scripts/catController.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum States
+{
+    Idle = 0,
+    Running = 1,
+    Jumping = 2,
+    Falling = 3,
+    Sliding = 4,
+    Dead = 5
+}
+public class catController : MonoBehaviour
+{
+
+    public Rigidbody2D rig;
+    public Animator anim;
+    public BoxCollider2D boxCollider;
+    public States state;
+    public LayerMask groundLayer;
+    float horizontalInput;
+
+    public float jumpVerticalPushOff;
+    Vector2 savedlocalScale;
+
+    float horizonatlSpeed = 2;
+
+    bool grounded = true;
+
+    public int maxSlideTimer = 0;
+    private int currentSlideTimer = 0;
+    bool goingRight = true;
+    private int slideCooldownTimer = 0;
+    private int maxSlideCooldown = 150;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        savedlocalScale = transform.localScale;
+    }
+
+    private void FixedUpdate()
+    {
+        if(currentSlideTimer > 0)
+        {
+            currentSlideTimer = currentSlideTimer - 1;
+        }
+        if(slideCooldownTimer > 0)
+        {
+             slideCooldownTimer = slideCooldownTimer - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        // what do the below lines do ?
+        if (horizontalInput > 0.001f)
+        {           
+            if(grounded == true && currentSlideTimer == 0)
+            {
+                transform.localScale = new Vector2(savedlocalScale.x, savedlocalScale.y);
+                goingRight = true;
+            }
+            
+        }
+        else if (horizontalInput < -0.001f)
+        {          
+            if (grounded == true && currentSlideTimer == 0)
+            {
+                transform.localScale = new Vector2(-savedlocalScale.x, savedlocalScale.y);//flips scale depending on direction
+                goingRight = false;
+            }                
+        }
+
+        if(grounded == true)
+        {
+            state = States.Idle;
+        }
+        if (currentSlideTimer > 0)
+        {
+            state = States.Sliding;
+        }
+
+        if (state == States.Idle && grounded == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                state = States.Jumping;
+                Debug.Log("Space Pressed");
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                state = States.Running;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                state = States.Running;
+            }
+            if (Input.GetKey(KeyCode.Space) && grounded == true)
+            {
+                rig.velocity = new Vector2(0.0f, jumpVerticalPushOff);
+                state = States.Jumping;
+                grounded = false;
+            }
+        }
+
+        if (state == States.Running)
+        {
+            if(Input.GetKey(KeyCode.LeftShift) && currentSlideTimer == 0 && slideCooldownTimer == 0)
+            {
+                slideCooldownTimer = maxSlideCooldown;
+                currentSlideTimer = maxSlideTimer;
+                state = States.Sliding;
+            }
+            if(Input.GetKey(KeyCode.Space) && grounded == true)
+            {
+                rig.velocity = new Vector2(0.0f, jumpVerticalPushOff);
+                state = States.Jumping;
+                grounded = false;
+            }
+        }
+
+        if (state == States.Jumping)
+        {
+            if (goingRight == true)
+            {
+                horizontalInput = 1.0f;
+            }
+            if (goingRight == false)
+            {
+                horizontalInput = -1.0f;
+            }
+            Vector2 tempVelocity = rig.velocity;
+            tempVelocity.y = tempVelocity.y * 0.996f;
+            if(tempVelocity.y <=0.1f)
+            {
+                tempVelocity.y = 0.1f;
+                state = States.Falling;
+            }
+            rig.velocity = tempVelocity;
+            //test if the cat is moving down the screen. Change state to falling
+            //If it hits the ground then state is idle
+        }
+        if (state == States.Sliding)
+        {
+            if (goingRight == true)
+            {
+                horizontalInput = 1.0f;
+            }
+            if (goingRight == false)
+            {
+                horizontalInput = -1.0f;
+            }
+            //If moving and left shift is not pressed then run
+            //it no input idle
+
+        }
+        if (state == States.Falling)
+        {
+            if (goingRight == true)
+            {
+                horizontalInput = 1.0f;
+            }
+            if (goingRight == false)
+            {
+                horizontalInput = -1.0f;
+            }
+            Vector2 tempVelocity = rig.velocity;
+            tempVelocity.y = tempVelocity.y * 1.007f;
+            if(tempVelocity.y > 0)
+            {
+                tempVelocity.y = -tempVelocity.y;
+            }
+            rig.velocity = tempVelocity;
+            //If moving and left shift is not pressed then run
+            //it no input idle
+
+        }
+
+        if (state == States.Dead)
+        {
+
+        }
+
+        rig.velocity = new Vector2(horizontalInput * horizonatlSpeed, rig.velocity.y);
+
+        anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            state = States.Idle;
+            //use a grounded bool
+            grounded = true;
+        }
+    }
+}
