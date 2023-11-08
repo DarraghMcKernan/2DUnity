d00d543e4e65146d68d9c8706401e8b21ed57b32
DarraghMcKernan <dmckernan77@gmail.com>
2023-11-08 15:58:09 +0000

    pushing before end of class

---
+++ Lab Project Hand Drawn Game/Assets/Scipts/BulletMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    public int speed = 1;
+    public int deletionTimer = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float rotation = transform.eulerAngles.z;
+
+        rotation = rotation + 90.0f;
+        float angleInRadians = rotation * Mathf.Deg2Rad;
+
+        Vector2 bulletDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
+
+        GetComponent<Rigidbody2D>().velocity = bulletDirection * speed;
+
+        deletionTimer--;
+        if(deletionTimer <0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Lab Project Hand Drawn Game/Assets/Scipts/PlayerController.cs
+++ Lab Project Hand Drawn Game/Assets/Scipts/PlayerController.cs
@@ -5,6 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private float movementX;
+    private float movementY;
+
+    public GameObject bulletPrefab;
+    private int bulletCooldownTimer = 0;
+    public int maxBulletCooldown = 10;
+
+    private Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector2 (movementX, movementY);
+
+        gameObject.transform.position = gameObject.transform.position + (movement/8);
+
+        bulletCooldownTimer--;
+        if (bulletCooldownTimer < 0)
+        {
+            bulletCooldownTimer = 0;
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(bulletPrefab, gameObject.transform.position, getRotationForBullet());
+                bulletCooldownTimer = maxBulletCooldown;
+            }
+        }
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private Quaternion getRotationForBullet()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        return Quaternion.Euler(0, 0, angle-90);
     }
 }
